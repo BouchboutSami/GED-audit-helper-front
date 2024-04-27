640537ace8df266b2207fb7d1768d33a83f17568
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-04-27 05:24:52 +0100

    all projets + single projet

--- src/app/ProjectDetail/[ProjectNumber]/page.jsx
+++ src/app/ProjectDetail/[ProjectNumber]/page.jsx
@@ -1,50 +1,112 @@
 "use client";
 import Sidebar from "@/components/Sidebar";
 import React, { useState } from "react";
+import { IoCloudUploadOutline } from "react-icons/io5";
 
 const page = ({ params }) => {
-  const [Project, setProject] = useState({});
+  const [project, setproject] = useState({});
+  const [pdfActive, setpdfActive] = useState(false);
+  const [resultPDFActive, setresultPDFActive] = useState(false);
+  const [rapport, setrapport] = useState(null);
+  const [circuitActive, setcircuitActive] = useState(false);
   // params.ProjectNumber = id tae l project
+  function handleChange(e) {
+    const file = e.target.files[0];
+    setrapport(file);
+    var fileReader = new FileReader();
+    var base64File;
+    fileReader.onload = function (event) {
+      base64File = event.target.result;
+      // send base64 to backend et f le last then =>
+    };
+  }
+  // function handleSuivant() {
+  //   if (rapport != null) {
+  //     setpdfActive(false);
+  //     setresultPDFActive(true);
+  //   }
+  // }
   return (
-    <div className="w-full flex flex-row">
-      <Sidebar />
-      <div className="right  w-auto pl-[5%] pr-[10%] py-[5%] flex flex-col">
-        <h1 className="text-4xl font-semibold mb-8">
-          Projet GED {params.ProjectNumber}
-        </h1>
-        <h2 className="text-xl font-semibold mb-1">Description :</h2>
-        <p className="text-[#575757] mb-8">
-          Le projet GED A concerne la digitalisation de la circulation des
-          documents, tout en garantissant une optimisation du workflow. Le
-          projet GED A concerne la digitalisation de la circulation des
-          documents, tout en garantissant une optimisation du workflow.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">Trucs</h2>
-        <div className="flex gap-10 flex-wrap">
-          <div className="card cursor-pointer flex flex-col bg-white px-[2%] py-4 gap-4 rounded-lg">
-            <div className="w-full h-auto">
-              <img
-                src="/images/audit.png"
-                alt=""
-                className="rounded-lg w-full"
-              />
+    <div className="relative">
+      <div
+        className="w-full flex flex-row"
+        style={{ opacity: pdfActive === false ? 1 : 0.3 }}
+      >
+        <Sidebar />
+        <div className="right  w-auto pl-[5%] pr-[10%] py-[5%] flex flex-col">
+          <h1 className="text-4xl font-semibold mb-8">
+            Projet GED {params.ProjectNumber}
+          </h1>
+          <h2 className="text-xl font-semibold mb-1">Description :</h2>
+          <p className="text-[#575757] mb-8">
+            Le projet GED A concerne la digitalisation de la circulation des
+            documents, tout en garantissant une optimisation du workflow. Le
+            projet GED A concerne la digitalisation de la circulation des
+            documents, tout en garantissant une optimisation du workflow.
+          </p>
+          <h2 className="text-xl font-semibold mb-3">Trucs</h2>
+          <div className="flex gap-10 flex-wrap">
+            <div
+              className="card cursor-pointer flex flex-col bg-white px-[2%] py-4 gap-4 rounded-lg hover:scale-105"
+              onClick={() => {
+                setpdfActive(!pdfActive);
+              }}
+            >
+              <div className="w-full h-auto">
+                <img
+                  src="/images/audit.png"
+                  alt=""
+                  className="rounded-lg w-full"
+                />
+              </div>
+              <h2 className="w-4/5">
+                Examiner le rapport d’audit documentaire
+              </h2>
             </div>
-            <h2 className="w-4/5">Examiner le rapport d’audit documentaire</h2>
-          </div>
-          <div className="card cursor-pointer flex flex-col bg-white px-[2%] py-4 gap-4 rounded-lg">
-            <div className="w-full h-auto">
-              <img
-                src="/images/circuit.png"
-                alt=""
-                className="rounded-lg w-full"
-              />
+            <div className="card cursor-pointer flex flex-col bg-white px-[2%] py-4 gap-4 rounded-lg hover:scale-105">
+              <div className="w-full h-auto">
+                <img
+                  src="/images/circuit.png"
+                  alt=""
+                  className="rounded-lg w-full"
+                />
+              </div>
+              <h2 className="w-4/5">
+                Examiner un circuit de validation (workflow)
+              </h2>
             </div>
-            <h2 className="w-4/5">
-              Examiner un circuit de validation (workflow)
-            </h2>
           </div>
         </div>
       </div>
+      {pdfActive && (
+        <div className="absolute w-[40vw] bg-white top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 rounded-xl flex flex-col items-center justify-center gap-10 py-10">
+          <div className="w-4/5 border-2 border-dashed flex flex-col justify-center items-center h-auto relative gap-4 py-4">
+            <IoCloudUploadOutline size={100} />
+            <h2>
+              Drag&Drop le rapport ou{" "}
+              <span className="border-bottom-2">l'importer</span>
+            </h2>
+            <p>Le rapport d'audit va etre examiné</p>
+            <input
+              type="File"
+              className="w-full h-full block opacity-0 absolute"
+              accept="application/pdf"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          {rapport != null && <p>{rapport.name}</p>}
+          <button className="px-6 py-2 flex items-center justify-center bg-black text-white rounded-lg">
+            Suivant
+          </button>
+        </div>
+      )}
+      {resultPDFActive && (
+        <div className="absolute w-[40vw] bg-white top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 rounded-xl flex flex-col items-center justify-center gap-10 py-10">
+          <button className="px-6 py-2 flex items-center justify-center bg-black text-white rounded-lg">
+            Voir détails
+          </button>
+        </div>
+      )}
     </div>
   );
 };
